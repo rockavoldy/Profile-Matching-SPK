5a3d81eec68b86e574a2aa088b7ad0630c18ab99
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2019-06-30 06:18:41 +0700

    last hitung SPK

--- application/controllers/HitungController.php
+++ application/controllers/HitungController.php
@@ -4,5 +4,160 @@ require APPPATH . 'libraries/REST_Controller.php';
 class HitungController extends REST_Controller
 {
     public function index_get()
-    { }
+    {
+        // penyederhanaan nilai input user
+        $data = $this->setNilaiAspek();
+        if ($data) {
+            $response = array(
+                'status' => 200,
+                'data' => $data
+            );
+        } else {
+            $response = array(
+                'status' => 403,
+                // 'message' => 'Belum ada aspek yang ditambahkan !'
+            );
+        }
+
+        $this->response($response, $response['status']);
+    }
+
+    private function getDataKelas()
+    {
+        $data = $this->hitung->getMinMaxRaw();
+        if ($data) {
+            foreach ($data as $dat) {
+                $dat['int_kelas'] = (int) $dat['int_kelas'];
+                $dat['jangkauan'] = (int) $dat['jangkauan'];
+                $dat['min_raw'] = (int) $dat['min_raw'];
+                $dat['max_raw'] = (int) $dat['max_raw'];
+                for ($i = 1; $i <= 9; $i++) {
+                    if ($i == 1) {
+                        $dat['kelas' . $i] = $dat['min_raw'] + $dat['int_kelas'];
+                    } else {
+                        $dat['kelas' . $i] = ($dat['kelas' . ($i - 1)] + 1) + $dat['int_kelas'];
+                    }
+                }
+                $dataa[] = $dat;
+            }
+            return $dataa;
+        } else {
+            return false;
+        }
+    }
+
+    private function setNilaiKriteria()
+    {
+        $data_raw = $this->hitung->getRaw();
+        $data = $this->getDataKelas();
+        $nilai = 0;
+
+        foreach ($data as $kelas) {
+            foreach ($data_raw as $raw) {
+                $raw['raw_data'] = (int) $raw['raw_data'];
+                if ($raw['id_factor'] == $kelas['id_factor']) {
+                    for ($i = 1; $i <= 9; $i++) {
+                        if ($raw['raw_data'] <= $kelas['kelas' . $i]) {
+                            $nilai = $i;
+                            break;
+                        }
+                    }
+                    $data_nilai[] = array(
+                        'id_siswa' => $raw['id_siswa'],
+                        'id_factor' => $raw['id_factor'],
+                        'nilai' => $nilai
+                    );
+                }
+            }
+        }
+
+        return $data_nilai;
+        // $this->hitung->addNilaiFactor($data_nilai);
+    }
+
+    private function setGapNilaiFactor()
+    {
+        $nilaiFactorSiswa = $this->setNilaiKriteria();
+        $nilaiFactor = $this->hitung->getNilai();
+
+        foreach ($nilaiFactor as $nilai) {
+            $gap = (int) ($nilai['nilai'] - $nilai['nilai_factor']);
+            if ($gap < -4) $bobot = 1;
+            if ($gap > 4) $bobot = 5;
+            switch ($gap) {
+                case 0:
+                    $bobot = 3;
+                    break;
+                case 1:
+                    $bobot = 3.5;
+                    break;
+                case -1:
+                    $bobot = 2.5;
+                    break;
+                case 2:
+                    $bobot = 4;
+                    break;
+                case -2:
+                    $bobot = 2;
+                    break;
+                case 3:
+                    $bobot = 4.5;
+                    break;
+                case -3:
+                    $bobot = 1.5;
+                    break;
+                case 4:
+                    $bobot = 5;
+                    break;
+                case -4:
+                    $bobot = 1;
+                    break;
+                default:
+                    $bobot = 3;
+                    break;
+            }
+
+            $data[] = array(
+                'id_siswa' => $nilai['id_siswa'],
+                'id_factor' => $nilai['id_factor'],
+                'gap' => $gap,
+                'bobot' => $bobot
+            );
+        }
+
+        $this->hitung->addNilaiFactor($nilaiFactorSiswa);
+        $this->hitung->addGapNilaiFactor($data);
+    }
+
+    private function setNilaiAspek()
+    {
+        $this->setGapNilaiFactor();
+        $data = $this->hitung->getAspek();
+
+        // foreach ($data as $siswa) {
+        //     foreach ($siswa['data'] as $aspek) {
+        //         $NCF = 0;
+        //         $NSF = 0;
+        //         $TCF = 0;
+        //         $TSF = 0;
+        //         foreach ($aspek['data'] as $factor) {
+        //             if ($factor['jenis'] == 'core') {
+        //                 $NCF += $factor['bobot'];
+        //                 $TCF++;
+        //             } else {
+        //                 $NSF += $factor['bobot'];
+        //                 $TSF++;
+        //             }
+        //         }
+        //     }
+        //     $dat[] = array(
+        //         'id_siswa' => $siswa['id_siswa'],
+        //         'NCF' => $NCF,
+        //         'NSF' => $NSF,
+        //         'TCF' => $TCF,
+        //         'TSF' => $TSF
+        //     );
+        // }
+        return $data;
+    }
 }
--- application/models/HitungModel.php
+++ application/models/HitungModel.php
@@ -1,3 +1,92 @@
 <?php
 class HitungModel extends CI_Model
-{ }
+{
+    function getRaw()
+    {
+        $data = $this->db->get('tb_raw_factor');
+        return $data->result_array();
+    }
+
+    function getMinMaxRaw()
+    {
+        $this->db->select('id_factor, min(raw_data) as min_raw, max(raw_data) as max_raw');
+        $this->db->select('(max(raw_data) - min(raw_data)) as jangkauan');
+        $this->db->select('((max(raw_data) - min(raw_data))/9) as int_kelas');
+        $this->db->from('tb_raw_factor');
+        $this->db->group_by('id_factor');
+        $data = $this->db->get();
+        return $data->result_array();
+    }
+
+    function getNilai()
+    {
+        $this->db->select('tb_nilai_factor.id_factor, tb_nilai_factor.id_siswa, tb_factor.id_aspek, tb_nilai_factor.nilai');
+        $this->db->select('tb_factor.nilai as nilai_factor');
+        $this->db->from('tb_nilai_factor');
+        $this->db->join('tb_factor', 'tb_factor.id = tb_nilai_factor.id_factor', 'left');
+        $data = $this->db->get();
+        return $data->result_array();
+    }
+
+    function addNilaiFactor($data)
+    {
+        $this->db->empty_table('tb_nilai_factor');
+        if ($this->db->insert_batch('tb_nilai_factor', $data)) {
+            return true;
+        }
+        return false;
+    }
+
+    function addGapNilaiFactor($data)
+    {
+        $this->db->empty_table('tb_gap_nilai_factor');
+        if ($this->db->insert_batch('tb_gap_nilai_factor', $data)) {
+            return true;
+        }
+        return false;
+    }
+
+    function getAspek()
+    {
+        $this->db->select('id_siswa');
+        $this->db->group_by('id_siswa');
+        $siswa = $this->db->get('tb_gap_nilai_factor');
+        $data_siswa = $siswa->result_array();
+
+        foreach ($data_siswa as $sw) {
+            $this->db->select('tb_aspek.id as id_aspek, tb_aspek.persentase');
+            $this->db->from('tb_gap_nilai_factor');
+            $this->db->join('tb_factor', 'tb_gap_nilai_factor.id_factor = tb_factor.id', 'left');
+            $this->db->join('tb_aspek', 'tb_factor.id_aspek = tb_aspek.id', 'left');
+            $this->db->where('id_siswa', $sw['id_siswa']);
+            $this->db->group_by('tb_aspek.id');
+            $aspek = $this->db->get();
+            $data_aspek = $aspek->result_array();
+            $ds = $data_aspek;
+
+            foreach ($ds as $da) {
+                $this->db->select('id_factor, jenis, bobot');
+                $this->db->from('tb_gap_nilai_factor');
+                $this->db->join('tb_factor', 'tb_gap_nilai_factor.id_factor = tb_factor.id', 'left');
+                $this->db->join('tb_aspek', 'tb_factor.id_aspek = tb_aspek.id', 'left');
+                $this->db->where('id_siswa', $sw['id_siswa']);
+                $this->db->where('id_aspek', $da['id_aspek']);
+                $factor = $this->db->get();
+                $data_factor = $factor->result_array();
+
+                // $data_factor['id_aspek'] = $da['id'];
+                $da['data'] = $data_factor;
+                // $data_aspek['data'] = $data_aspek;
+                // $data_factor['data'] = $data_factor;
+                $dataa[] = array(
+                    'id_siswa' => $sw['id_siswa'],
+                    'data' => $da
+                );
+            }
+        }
+
+
+        // return $data->result_array();
+        return $dataa;
+    }
+}
